639a65a4c11457e223ac0d63d533b11d17d25119
Joshua Jones <49686077+JoshuaJoness@users.noreply.github.com>
2020-05-07 23:20:44 -0400

    Revert "added env vars, prepare for deployment"
    This reverts commit 860dd39203faf9cb993ecc363a327c87b70c5639.

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-require("dotenv").config();
+const PORT = 4002 || process.env.PORT
 
-const PORT = process.env.PORT
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/zenchat', {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+  err ? console.log(err) : console.log('Connected to MongoDB')
+})
 
 // Connect to SocketIO
 io.on('connection', (socket) => {
@@ -30,35 +33,23 @@ io.on('connection', (socket) => {
   })
 });
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 app.use(cors({credentials: true}))
 
 // Routes
 app.get('/', (req, res) => {
   res.send('Welcome to the ZenChat server');
 });
+
 app.get('/messages', require('./controllers/getMessages'))
 app.post('/message', require('./controllers/postMessage'));
 
-// Server
 server.listen(PORT, () => {
   console.log(`Ready on PORT ${PORT}`);
 })
 
-// Database 
-let mongoUri = ''
-
-if (PORT == 4002) {
-  mongoUri = 'mongodb://localhost:27017/zenchat'
-} else {
-    mongoUri = `mongodb+srv://joshuajoness:${process.env.PASSWORD}@cluster0-4cwwp.mongodb.net/test?retryWrites=true&w=majority`
-  }
-
-mongoose.connect('mongodb://localhost:27017/zenchat', {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-  err ? console.log(err) : console.log('Connected to MongoDB')
-})
 
 
 
